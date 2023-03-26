58ad93fe421139d0a81560065fa6c93ffcac1365
Ashu sharma <128977501+imdivyanshu2003@users.noreply.github.com>
2023-03-26 23:06:19 +0530

    Again Update App.js

--- app.js
+++ app.js
@@ -137,6 +137,10 @@ app.get("/:customListName", async function (req, res) {
       console.log(err);
     });
 }),
-  app.listen(3000, (req, res) => {
-    console.log(`Example app listening on port 3000`);
-  });
+  app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function() {
+
+    console.log('Server started on port '+app.get('port'));
+
+});
